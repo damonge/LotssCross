0485b40a7571d50cbf3257c7828c5ea6aeb26a1f
David Alonso <dam.phys@gmail.com>
2020-03-24 12:00:49 +0000

    flux pdf

--- utils.py
+++ utils.py
@@ -1,6 +1,7 @@
 import wget
 import os
-
+import matplotlib.pyplot as plt
+import numpy as np
 
 def get_pointing_names():
     with open("data/pointing_names.txt") as f:
@@ -16,3 +17,30 @@ def dwl_file(url, fname_out, redwl=False, verbose=True):
     else:
         if verbose:
             print(f"Found {fname_out}")
+
+
+class FluxPDF(object):
+    def __init__(self, fname_in="data/skads_flux_counts.result"):
+        from scipy.interpolate import interp1d
+        # Read flux distribution from SKADS' S3-SEX simulation
+        log_flux, counts = np.loadtxt(fname_in, unpack=True,
+                                      delimiter=',', skiprows=1)
+        log_flux += 3 # Use mJy instead of Jy
+        # Cut to non-zero counts
+        log_flux = log_flux[counts >= 0]
+        counts = counts[counts >= 0]
+        self.lpdf = interp1d(log_flux, np.log10(counts),
+                             fill_value=-500, bounds_error=False)
+
+    def plot_pdf(self, log_flux_min=-6, log_flux_max=6,
+                 n_log_flux=256):
+        lf = np.linspace(log_flux_min, log_flux_max, n_log_flux)
+        dlf = np.mean(np.diff(lf))
+        p = 10.**self.lpdf(lf)
+        p /= np.sum(p) * dlf
+        plt.figure()
+        plt.plot(10.**lf, p, 'k-')
+        plt.loglog()
+        plt.xlabel(r'$I_{1400}\,{\rm mJy}$', fontsize=14)
+        plt.ylabel(r'$dp/d\log_{10}I_{1400}$', fontsize=14)
+        plt.show()
