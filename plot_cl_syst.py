95b010dd0991a9ee88daa318fbe01a017e6809a8
David Alonso <dam.phys@gmail.com>
2020-05-06 15:13:55 +0100

    flake

--- plot_cl_syst.py
+++ plot_cl_syst.py
@@ -16,27 +16,29 @@ def plot_comp(dirs, fname_out=None):
     ds = [np.load(os.path.join(d['dirname'], 'cls.npz')) for d in dirs]
     size = np.amin([len(d['cl_gg']) for d in ds])
     for i, d in enumerate(ds[1:]):
-        l = d['l_eff'][:size]
-        dl = -6 + 12 * i /(ndir-1)
+        ll = d['l_eff'][:size]
+        dl = -6 + 12 * i / (ndir-1)
         c = cols[i % 7]
         dx = ((d['cl_gg']-d['nl_gg'])[:size] -
               (ds[0]['cl_gg']-ds[0]['nl_gg'])[:size])
         er = np.sqrt(np.diag(np.fabs(ds[0]['cov_gggg'])))[:size]
-        ax1.errorbar(l + dl, dx / er, yerr=er/er,
+        ax1.errorbar(ll + dl, dx / er, yerr=er/er,
                      fmt=c+'.', label=dirs[i+1]['name'])
         dx = (d['cl_gk'][:size]-ds[0]['cl_gk'][:size])
         er = np.sqrt(np.diag(np.fabs(ds[0]['cov_gkgk'])))[:size]
-        ax2.errorbar(l + dl, dx / er, yerr=er/er,
+        ax2.errorbar(ll + dl, dx / er, yerr=er/er,
                      fmt=c+'.', label=dirs[i+1]['name'])
         ax1.plot([10, 512], [0, 0], 'k--', lw=1)
         ax2.plot([10, 512], [0, 0], 'k--', lw=1)
     ax2.set_xlabel(r'$\ell$', fontsize=16)
-    ax1.set_ylabel(r'$\Delta C^{gg}_\ell / \sigma_\ell^{gg}$', fontsize=16)
-    ax2.set_ylabel(r'$\Delta C^{g\kappa}_\ell / \sigma_\ell^{g\kappa}$', fontsize=16)
+    ax1.set_ylabel(r'$\Delta C^{gg}_\ell / \sigma_\ell^{gg}$',
+                   fontsize=16)
+    ax2.set_ylabel(r'$\Delta C^{g\kappa}_\ell / \sigma_\ell^{g\kappa}$',
+                   fontsize=16)
     for ax in [ax1, ax2]:
         ax.set_xlim([10, 512])
     ax1.legend(loc='upper right', frameon=False,
-              ncol=2, labelspacing=0.1)
+               ncol=2, labelspacing=0.1)
 
     if fname_out is not None:
         plt.savefig(fname_out, bbox_inches='tight')
