06b5f7a7a14e6f11329507797aa86b6a81ab99a3
David Alonso <dam.phys@gmail.com>
2020-07-06 09:53:02 +0100

    new sampling

--- sampler.py
+++ sampler.py
@@ -1,69 +1,31 @@
-import numpy as np
-import matplotlib.pyplot as plt
-import theory as th
-import os
-import yaml
+import utils as ut
+
+nz_choice='w'
+bz_choice='inv_growth'
+data=['gk']
+pars, fname = ut.mk_sampler_params('output_cls_cov/out_2048_pfull_hrmask_deproj/',
+                                   nz_choice=nz_choice, bz_choice=bz_choice,
+                                   nz_sample=False, bz_sample=True, s8_sample=False,
+                                   data=data, nsteps=3000, nwalkers=6, l_max=500)
+ut.sample(pars, fname, plot_stuff=False)
+exit(1)
+for nz_choice in ['w', 's3', 'vc']:
+    for bz_choice in ['inv_growth', 'constant']:
+        for data in [['gg'], ['gk'], ['gg', 'gk']]:
+            print(nz_choice, bz_choice, data)
+            pars, fname = ut.mk_sampler_params('output_cls_cov/out_2048_pfull_hrmask_deproj/',
+                                               nz_choice=nz_choice, bz_choice=bz_choice,
+                                               nz_sample=False, bz_sample=True, s8_sample=False,
+                                               data=data, nsteps=3000, nwalkers=6, l_max=500)
+            ut.sample(pars, fname, plot_stuff=False)
 
 
-fname_pars = 'params.yml'
-with open(fname_pars) as f:
-    pyml = yaml.safe_load(f)
-
-like = th.Like(pyml)
-
-cl_d = like.get_data(split=True)
-cov = like.get_covar(split=True)
-
-# Best-fit and Fisher matrix
-cl_t_ini = like.get_theory(like.p0, split=True)
-if os.path.isfile(like.prefix_out+'_p0.npzzzz'):
-    d = np.load(like.prefix_out+'_p0.npz')
-    like.update_p0(d['p0'])
-    fisher = d['fisher']
-else:
-    like.get_best_fit(update_p0=True)
-    fisher = like.get_Fisher()
-    np.savez(like.prefix_out+'_p0.npz',
-             p0=like.p0, fisher=fisher)
-print(like.p0,np.sqrt(np.diag(np.linalg.inv(fisher))))
-cl_t_end = like.get_theory(like.p0, split=True)
-
-# MCMC
-like.sample()
-
-# Plotting
-like.get_chain()
-plt.figure()
-for p in like.chain[::10]:
-    par = like.build_kwargs(p)
-    z, nz = like.model.get_nz(**par)
-    plt.plot(z, nz/np.amax(nz),'r-', alpha=0.05)
-d = np.load('output_cls_cov/out_2048_pfull_hrmask_deproj/nz.npz')
-plt.plot(d['z_g'], d['nz_g_w']/np.amax(d['nz_g_w']), 'k-', label='LoTSS')
-plt.plot(d['z_g'], d['nz_g_s3']/np.amax(d['nz_g_s3']), 'k--', label='SKADS')
-plt.plot(d['z_g_vc'], d['nz_g_vc']/np.amax(d['nz_g_vc']), 'k-.', label='VLA-COSMOS')
-plt.xlim([0, 5])
-plt.legend(loc='lower left', fontsize=15)
-plt.xlabel(r'$z$', fontsize=15)
-plt.ylabel(r'$N(z)$', fontsize=15)
-plt.yscale('log')
-plt.savefig(like.prefix_out+'nzs.pdf',
-            bbox_inches='tight')
-
-for i, (d, t_i, t_f) in enumerate(zip(cl_d, cl_t_ini, cl_t_end)):
-    err = np.sqrt(np.fabs(np.diag(cov[i, :, i, :])))
-    plt.figure()
-    plt.title(like.cl_list[i])
-    plt.errorbar(like.l, like.l*d, yerr=like.l*err, fmt='k.', label='Data')
-    plt.plot(like.l, like.l*t_i, 'r-', label='Guess')
-    plt.plot(like.l, like.l*t_f, 'b-', label='Best-fit')
-    plt.plot(like.l, 0*like.l, 'k--')
-    plt.xlabel(r'$\ell$', fontsize=16)
-    plt.ylabel(r'$\ell\,C_\ell$', fontsize=16)
-    plt.savefig(like.prefix_out+'cl_'+like.cl_list[i]+'.pdf',
-                bbox_inches='tight')
-print(like.p0, np.sqrt(np.diag(np.linalg.inv(fisher))),
-      like.chi2(like.p0), like.chi2(like.p0)/like.nd_tot)
-
-plt.show()
-like.plot_chain(like.chain, save_figure=True)
+for nz_choice in ['ana', 'w', 's3', 'vc']:
+    for bz_choice in ['inv_growth', 'constant']:
+        data = ['gg', 'gk']
+        print(nz_choice, bz_choice, data)
+        pars, fname = ut.mk_sampler_params('output_cls_cov/out_2048_pfull_hrmask_deproj/',
+                                           nz_choice=nz_choice, bz_choice=bz_choice,
+                                           nz_sample=True, bz_sample=True, s8_sample=False,
+                                           data=data, nsteps=10000, nwalkers=6, l_max=500)
+        ut.sample(pars, fname, plot_stuff=True)
--- utils.py
+++ utils.py
@@ -321,3 +321,176 @@ def write_to_fits(fname, array, name, type='image', header=None):
     print('Appended ' + name.upper() + ' to ' + os.path.relpath(fname))
     sys.stdout.flush()
     return
+
+
+def get_params_sampler_default():
+    return [{'name': 'bias',
+             'label': 'b_g',
+             'vary': False,
+             'value': 1.3,
+             'prior': {'type': 'TopHat',
+                       'values': [0.6, 6.0]}},
+            
+            {'name': 'sigma8',
+             'label': '\\sigma_8',
+             'vary': False,
+             'value': 0.81,
+             'prior': {'type': 'TopHat',
+                       'values': [0.2, 2.0]}},
+            {'name': 'width',
+             'label': 'w_z',
+             'vary': False,
+             'value': 1.0,
+             'prior': {'type': 'TopHat',
+                       'values': [0.01, 6.]}},
+            {'name': 'z_tail',
+             'label': 'z_{\\rm tail}',
+             'vary': False,
+             'value': 0.8,
+             'prior': {'type': 'TopHat',
+                       'values': [0.1, 5]}},
+            {'name': 'z_ini',
+             'label': 'z_{\rm ini}',
+             'vary': False,
+             'value': 0.001,
+             'prior': {'type': 'TopHat',
+                       'values': [0.1, 0.1]}},
+            {'name': 'gamma',
+             'label': '\gamma',
+             'vary': False,
+             'value': 3.5,
+             'prior': {'type': 'TopHat',
+                       'values': [3.5, 3.5]}}]
+
+
+def mk_sampler_params(prefix_in, nz_choice, bz_choice, nz_sample, bz_sample,
+                      s8_sample=False, data=['gg', 'gk'], nsteps=3000, nwalkers=6,
+                      l_max=500):
+    p = {}
+    p['l_max'] = l_max
+    p['cls_include'] = data
+    p['cl_file'] = prefix_in+'cls.npz'
+    p['nz'] = {}
+    if nz_choice == 'ana':
+        p['nz']['nz_source'] = 'analytic'
+    else:
+        p['nz']['nz_source'] = prefix_in+'nz.npz'
+        if nz_choice == 'vc':
+            p['nz']['z_name'] = 'z_g_vc'
+        else:
+            p['nz']['z_name'] = 'z_g'
+        p['nz']['nz_name'] = 'nz_g_' + nz_choice
+
+    p['bias'] = {'bias_type': bz_choice}
+    p['params'] = get_params_sampler_default()
+    p['sampler'] = {'nwalkers': nwalkers,
+                    'nsteps': nsteps,
+                    'rerun': False}
+
+    if nz_sample:
+        if nz_choice == 'ana':
+            p['params'][3]['vary'] = True
+        else:
+            p['params'][2]['vary'] = True
+
+    if s8_sample:
+        p['params'][1]['vary'] = True
+
+    if bz_sample:
+        p['params'][0]['vary'] = True
+    
+    fname = 'sampler_'
+    fname += 'nz'+nz_choice+'_'
+    if bz_choice == 'inv_growth':
+        fname += 'bgrth_'
+    else:
+        fname += 'bcnst_'
+    if nz_sample:
+        fname += 'nzvary_'
+    else:
+        fname += 'nzfixed_'
+    if bz_sample:
+        fname += 'bzvary_'
+    else:
+        fname += 'bzfixed_'
+    if s8_sample:
+        fname += 's8vary_'
+    else:
+        fname += 's8fixed_'
+    for d in data:
+        fname += d
+    fname += '_'
+
+    p['output_prefix'] = prefix_in + fname
+    p_fname = prefix_in + fname+'params.yml'
+    return p, p_fname
+
+
+def sample(pars, fname, plot_stuff=False):
+    import theory as th
+    import yaml
+
+    with open(fname, 'w') as fout:
+        yaml.dump(pars, fout, default_flow_style=False)
+
+    like = th.Like(pars)
+
+    cl_d = like.get_data(split=True)
+    cov = like.get_covar(split=True)
+
+    # Best-fit and Fisher matrix
+    cl_t_ini = like.get_theory(like.p0, split=True)
+    if os.path.isfile(like.prefix_out+'p0.npz'):
+        d = np.load(like.prefix_out+'p0.npz')
+        like.update_p0(d['p0'])
+        fisher = d['fisher']
+    else:
+        like.get_best_fit(update_p0=True)
+        fisher = like.get_Fisher()
+        np.savez(like.prefix_out+'p0.npz',
+                 p0=like.p0, fisher=fisher)
+
+    cl_t_end = like.get_theory(like.p0, split=True)
+    print(like.p0, np.sqrt(np.diag(np.linalg.inv(fisher))),
+          like.chi2(like.p0), like.chi2(like.p0)/like.nd_tot)
+
+    # MCMC
+    like.sample()
+
+    # Plotting
+    like.get_chain()
+    if plot_stuff:
+        plt.figure()
+        for p in like.chain[::10]:
+            par = like.build_kwargs(p)
+            z, nz = like.model.get_nz(**par)
+            plt.plot(z, nz/np.amax(nz),'r-', alpha=0.05)
+        d = np.load('output_cls_cov/out_2048_pfull_hrmask_deproj/nz.npz')
+        plt.plot(d['z_g'], d['nz_g_w']/np.amax(d['nz_g_w']), 'k-', label='LoTSS')
+        plt.plot(d['z_g'], d['nz_g_s3']/np.amax(d['nz_g_s3']), 'k--', label='SKADS')
+        plt.plot(d['z_g_vc'], d['nz_g_vc']/np.amax(d['nz_g_vc']), 'k-.', label='VLA-COSMOS')
+        plt.xlim([0, 5])
+        plt.legend(loc='lower left', fontsize=15)
+        plt.xlabel(r'$z$', fontsize=15)
+        plt.ylabel(r'$N(z)$', fontsize=15)
+        plt.yscale('log')
+        plt.savefig(like.prefix_out+'nzs.pdf',
+                    bbox_inches='tight')
+
+        for i, (d, t_i, t_f) in enumerate(zip(cl_d, cl_t_ini, cl_t_end)):
+            err = np.sqrt(np.fabs(np.diag(cov[i, :, i, :])))
+            plt.figure()
+            plt.title(like.cl_list[i])
+            plt.errorbar(like.l, d, yerr=err, fmt='k.', label='Data')
+            plt.plot(like.l, t_i, 'r-', label='Guess')
+            plt.plot(like.l, t_f, 'b-', label='Best-fit')
+            plt.plot(like.l, 0*like.l, 'k--')
+            plt.xlabel(r'$\ell$', fontsize=16)
+            plt.ylabel(r'$\ell\,C_\ell$', fontsize=16)
+            plt.savefig(like.prefix_out+'cl_'+like.cl_list[i]+'.pdf',
+                        bbox_inches='tight')
+        plt.show()
+        like.plot_chain(like.chain, save_figure=True)
+
+    print(like.p0, np.sqrt(np.diag(np.linalg.inv(fisher))),
+          like.chi2(like.p0), like.chi2(like.p0)/like.nd_tot)
